d2764529d561f8196285c271212bc2c00a2d47fe
Vanna Kruy <kruyvanna@gmail.com>
2020-04-28 13:24:37 +0700

    beatify Login page

--- src/app/Auth/Login.js
+++ src/app/Auth/Login.js
@@ -21,5 +21,45 @@ export const Login = () => {
     history.replace(from)
     // })
   }
-  return <button onClick={login}>Login</button>
+  // return <button onClick={login}>Login</button>
+  return (
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+              <form action="" class="box">
+                <div class="field">
+                  <label for="" class="label">
+                    Username
+                  </label>
+                  <div class="control has-icons-left">
+                    <input type="text" class="input" required />
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-envelope"></i>
+                    </span>
+                  </div>
+                </div>
+                <div class="field">
+                  <label for="" class="label">
+                    Password
+                  </label>
+                  <div class="control has-icons-left">
+                    <input type="password" class="input" required />
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <button class="button is-success">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
