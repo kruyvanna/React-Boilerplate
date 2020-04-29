69563ba73b2b12b82820d572f118244f6f631db2
Vanna Kruy <kruyvanna@gmail.com>
2020-04-29 21:34:25 +0700

    update Spinner

--- src/app/Components/Spinner.js
+++ src/app/Components/Spinner.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 export const Spinner = () => {
   return (
-    <div className="container">
+    <Container>
       <span class="icon has-text-success">
         <i class="fas fa-3x fa-spinner fa-pulse"></i>
       </span>
-    </div>
+    </Container>
   )
 }
