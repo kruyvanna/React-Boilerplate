ae9a0e2ded98ef56109b57ec9c2a22a0efb532bd
Vanna Kruy <kruyvanna@gmail.com>
2020-04-29 21:27:05 +0700

    add Spinner

---
+++ src/app/Components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Spinner = () => {
+  return (
+    <div className="container">
+      <span class="icon has-text-success">
+        <i class="fas fa-3x fa-spinner fa-pulse"></i>
+      </span>
+    </div>
+  )
+}
--- src/app/Components/index.js
+++ src/app/Components/index.js
@@ -1,1 +1,2 @@
 export * from './DeleteConfirmButton'
+export * from './Spinner'
